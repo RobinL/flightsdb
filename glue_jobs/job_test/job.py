0856b95cd9c8dfada45a6108701b2441c14b19fa
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-01-14 17:32:03 +0000

    first two tests

--- glue_jobs/job_test/job.py
+++ glue_jobs/job_test/job.py
@@ -12,20 +12,4 @@ glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
 flights_csv = spark.read.csv("s3://robintest-gluenotebooks3files/flights", header=True)
-print(flights_csv.take(5))
-print(flights_csv.show(2))
-
-print(flights_csv.sample(False, 0.0000001).show())
-
-flights_csv.registerTempTable('flights_csv')
-
-sql = "select * from flights_csv limit 10"
-sql_results = spark.sql(sql)
-print(sql_results.show())
-
-flights = glueContext.create_dynamic_frame.from_catalog(
-             database="flights",
-             table_name="flightsdata")
-flights_df = flights.toDF()
-
-print(flights_df.show(10))
+print(flights_csv.show(10))
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from dataengineeringutils import glue
 if __name__ == "__main__":
-    glue.delete_job("flights")
-    jobs_path = "s3://robintest-gluenotebooks3files/glue_jobs/"
-    glue.run_glue_job_from_local_folder_template("glue_jobs/job_test/", jobs_path, name="flights", role="robintest-gluerole", AllocatedCapacity = 5)
+    glue.delete_job("flights_a")
+    job_path = "s3://robintest-gluenotebooks3files/glue_jobs/job_test/"
+    glue.run_glue_job_from_local_folder_template("glue_jobs/job_test/", job_path, name="flights_a", role="robintest-gluerole", AllocatedCapacity = 5)
+    
+    glue.delete_job("flights_b")
+    job_path = "s3://robintest-gluenotebooks3files/glue_jobs/job_test2/"
+    glue.run_glue_job_from_local_folder_template("glue_jobs/job_test2/", job_path, name="flights_b", role="robintest-gluerole", AllocatedCapacity = 5)
